78b8245ce2a195aac1463640e9d5d79d2eb2e31b
jeffreyz <jeffreyz@q-free.com>
2019-09-12 15:22:56 +0200

    Updated delete_psa_data_from_etl_db.sql & delete_dsa_data_from_etl_db.sql for etl_mgmt data model changes

--- pdi_repository/dsa/metadata/delete_dsa_data_from_etl_db.sql
+++ pdi_repository/dsa/metadata/delete_dsa_data_from_etl_db.sql
@@ -15,12 +15,26 @@
 -- Author:   Jeffrey Zelt
 -- Changes:  Initial version
 
+SELECT COUNT(*) FROM etl.etl_run r WHERE etl_run_id IN (
+    SELECT 
+        DISTINCT etl_run_id 
+    FROM 
+        etl.target_table_update ttu 
+    INNER JOIN 
+        etl.table_meta tm ON tm.table_meta_id=ttu.table_meta_id WHERE                                                      tm.source_db_id=10 AND tm.target_db_id=20);
 SELECT COUNT(*) FROM etl.table_meta tm                                                                               WHERE tm.source_db_id=10 AND tm.target_db_id=20;
 SELECT COUNT(*) FROM etl.column_meta cm INNER JOIN etl.table_meta tm ON   cm.table_meta_id=tm.table_meta_id          WHERE tm.source_db_id=10 AND tm.target_db_id=20;
-SELECT tm.source_schema_name, tm.source_table_name, tm.target_schema_name, tm.target_table_name 
-                FROM etl.table_meta tm                                                                               WHERE tm.source_db_id=10 AND tm.target_db_id=20;
+SELECT COUNT(*) FROM  etl.table_meta tm                                                                              WHERE tm.source_db_id=10 AND tm.target_db_id=20;
 SELECT COUNT(*) FROM etl.target_table_update ttu  INNER JOIN etl.table_meta tm ON ttu.table_meta_id=tm.table_meta_id WHERE tm.source_db_id=10 AND tm.target_db_id=20;
 SELECT COUNT(*) FROM etl.target_table_compare ttc INNER JOIN etl.table_meta tm ON ttc.table_meta_id=tm.table_meta_id WHERE tm.source_db_id=10 AND tm.target_db_id=20;
+SELECT COUNT(*) FROM etl.target_table_progress ttp WHERE target_table_update_id IN (
+    SELECT 
+        target_table_update_id 
+    FROM 
+        etl.target_table_update ttu 
+    INNER JOIN 
+        etl.table_meta tm ON tm.table_meta_id=ttu.table_meta_id WHERE                                                      tm.source_db_id=10 AND tm.target_db_id=20);
+
 
 -- Delete column_meta rows before table_meta rows:
 DELETE FROM etl.column_meta cm USING etl.table_meta tm WHERE cm.table_meta_id=tm.table_meta_id AND                         tm.source_db_id=10 AND tm.target_db_id=20;
@@ -28,8 +42,26 @@ DELETE FROM etl.column_meta cm USING etl.table_meta tm WHERE cm.table_meta_id=tm
 -- Delete table_state rows before table_meta rows:
 DELETE FROM etl.table_state ts USING etl.table_meta tm WHERE ts.table_state_id=tm.table_meta_id AND                        tm.source_db_id=10 AND tm.target_db_id=20;
 
--- Delete target_table_update rows before table_meta rows:
-DELETE FROM etl.target_table_update ttu USING etl.table_meta tm  WHERE ttu.table_meta_id=tm.table_meta_id AND              tm.source_db_id=10 AND tm.target_db_id=20;
+---- Delete target_table_progress rows before target_table_update rows:
+--DELETE FROM etl.target_table_progress ttp WHERE target_table_update_id IN (
+--    SELECT 
+--        target_table_update_id 
+--    FROM 
+--        etl.target_table_update ttu 
+--    INNER JOIN 
+--        etl.table_meta tm ON tm.table_meta_id=ttu.table_meta_id WHERE                                                      tm.source_db_id=10 AND tm.target_db_id=20);
+
+-- Delete etl_run rows. This will also delete all related target_table_update & target_table_progress rows:
+DELETE FROM etl.etl_run r WHERE etl_run_id IN (
+    SELECT 
+        DISTINCT etl_run_id 
+    FROM 
+        etl.target_table_update ttu 
+    INNER JOIN 
+        etl.table_meta tm ON tm.table_meta_id=ttu.table_meta_id WHERE                                                      tm.source_db_id=10 AND tm.target_db_id=20);
+
+---- Delete target_table_update rows before table_meta rows:
+--DELETE FROM etl.target_table_update ttu USING etl.table_meta tm  WHERE ttu.table_meta_id=tm.table_meta_id AND              tm.source_db_id=10 AND tm.target_db_id=20;
 
 -- Delete target_table_compare rows before table_meta rows:
 DELETE FROM etl.target_table_compare ttc USING etl.table_meta tm WHERE ttc.table_meta_id=tm.table_meta_id AND              tm.source_db_id=10 AND tm.target_db_id=20;
@@ -37,9 +69,24 @@ DELETE FROM etl.target_table_compare ttc USING etl.table_meta tm WHERE ttc.table
 -- Delete table_meta rows last:
 DELETE FROM etl.table_meta tm                                                                                        WHERE tm.source_db_id=10 AND tm.target_db_id=20;
 
+
+-- These SELECTs should show zero COUNTs:
+SELECT COUNT(*) FROM etl.etl_run r WHERE etl_run_id IN (
+    SELECT 
+        DISTINCT etl_run_id 
+    FROM 
+        etl.target_table_update ttu 
+    INNER JOIN 
+        etl.table_meta tm ON tm.table_meta_id=ttu.table_meta_id WHERE                                                      tm.source_db_id=10 AND tm.target_db_id=20);
 SELECT COUNT(*) FROM etl.table_meta tm                                                                               WHERE tm.source_db_id=10 AND tm.target_db_id=20;
 SELECT COUNT(*) FROM etl.column_meta cm INNER JOIN etl.table_meta tm ON   cm.table_meta_id=tm.table_meta_id          WHERE tm.source_db_id=10 AND tm.target_db_id=20;
-SELECT tm.source_schema_name, tm.source_table_name, tm.target_schema_name, tm.target_table_name 
-                FROM etl.table_meta tm                                                                               WHERE tm.source_db_id=10 AND tm.target_db_id=20;
+SELECT COUNT(*) FROM  etl.table_meta tm                                                                              WHERE tm.source_db_id=10 AND tm.target_db_id=20;
 SELECT COUNT(*) FROM etl.target_table_update ttu  INNER JOIN etl.table_meta tm ON ttu.table_meta_id=tm.table_meta_id WHERE tm.source_db_id=10 AND tm.target_db_id=20;
 SELECT COUNT(*) FROM etl.target_table_compare ttc INNER JOIN etl.table_meta tm ON ttc.table_meta_id=tm.table_meta_id WHERE tm.source_db_id=10 AND tm.target_db_id=20;
+SELECT COUNT(*) FROM etl.target_table_progress ttp WHERE target_table_update_id IN (
+    SELECT 
+        target_table_update_id 
+    FROM 
+        etl.target_table_update ttu 
+    INNER JOIN 
+        etl.table_meta tm ON tm.table_meta_id=ttu.table_meta_id WHERE                                                      tm.source_db_id=10 AND tm.target_db_id=20);
